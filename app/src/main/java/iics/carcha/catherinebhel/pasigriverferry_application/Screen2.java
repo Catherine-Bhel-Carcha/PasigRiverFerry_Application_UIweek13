1d3486d5e1d9dbd99f1cda834d011e9886cae6a8
Catherine-Bhel-Carcha <2015081521@ust-ics.mygbiz.com>
2018-10-31 20:23:05 +0800

    Initial commit

---
+++ app/src/main/java/iics/carcha/catherinebhel/pasigriverferry_application/Screen2.java
@@ -0,0 +1,55 @@
+package iics.carcha.catherinebhel.pasigriverferry_application;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Screen2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_location); // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Stations, android.R.layout.simple_spinner_item); // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner_dropoff); // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Stations, android.R.layout.simple_spinner_item); // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
+    }
+    public void process(View v) {
+        Intent i = null, chooser = null;
+        if (v.getId() == R.id.submit) {
+            i = new Intent(this, Screen3.class);
+            startActivity(i);
+        }
+        // } else if (v.getId() == R.id.stations) {
+        //    i = new Intent(getIntent().ACTION_VIEW);
+        //  i.setData(Uri.parse("http://maps.google.com/maps?q=loc:" + 14.589595 + "," + 120.975579));
+        //chooser = Intent.createChooser(i, "Choose A Map App");
+        //startActivity(chooser);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
---
+++ app/src/main/java/iics/carcha/catherinebhel/pasigriverferry_application/Screen3.java
@@ -0,0 +1,27 @@
+package iics.carcha.catherinebhel.pasigriverferry_application;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Screen3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen3);
+    }
+    public void process(View v) {
+        Intent i = null, chooser = null;
+        if (v.getId() == R.id.other) {
+            i = new Intent(this, Screen2.class);
+            startActivity(i);
+        }
+        // } else if (v.getId() == R.id.stations) {
+        //    i = new Intent(getIntent().ACTION_VIEW);
+        //  i.setData(Uri.parse("http://maps.google.com/maps?q=loc:" + 14.589595 + "," + 120.975579));
+        //chooser = Intent.createChooser(i, "Choose A Map App");
+        //startActivity(chooser);
+    }
+}
